c1f42898b85a0234018cc66d47cb3a79d6e8c3c4
Robin Jan <loedje5@gmail.com>
2024-05-04 21:07:18 +0200

    reset project due to problems with new mc version 1.20.5

---
+++ src/client/java/loedje/jukebox/Jukebox_action_bar_fixClient.java
@@ -0,0 +1,10 @@
+package loedje.jukebox;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Jukebox_action_bar_fixClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
